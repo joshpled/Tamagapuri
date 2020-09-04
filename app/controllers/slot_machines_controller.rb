62b8c23d5ced7c7b7d5b05622ffbf84bd4e8366a
joshpled <joshuapleduc@gmail.com>
2020-09-03 22:45:37 -0400

    flash messages slot machines

--- app/controllers/slot_machines_controller.rb
+++ app/controllers/slot_machines_controller.rb
@@ -9,7 +9,7 @@ class SlotMachinesController < ApplicationController
     @slot = SlotMachine.new
     @array = @slot.outcomes
     @slot = SlotMachine.create(slot_1:@array[0],slot_2:@array[1],slot_3:@array[2])
-    flash[:won] = @slot.bid(current_user)
+    flash.now[:message] = @slot.bid(current_user)
     render 'show'
   end
 end
