06a1085395172edcf5e89d79576496389d0bd44c
joshpled <joshuapleduc@gmail.com>
2020-09-11 00:42:31 -0400

    refactor code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
 import 'bootstrap'
 import './src/application.scss'
 import './bootstrap_custom.js'
-import './items.js'
+
 
 $(document).ready(function(){
   $('.toast').toast({animation: true, delay: 2000});
   $('.toast').toast('show');
-  });
+  }); 
+
